1ee8119094a65cdea072b6486ee4d4179e07d369
杨茗凯 <yangmingkai@shuidihuzhu.com>
2021-04-27 23:28:10 +0800

    add jvm07

---
+++ main/JVM07HowReflect/README.md
@@ -0,0 +1,7 @@
+# 2021-04-26 JVM是如何实现反射的 HowJvmReflect
+
+## 反射调用的实现
+MethodAccessor
+- 本地实现
+- 委派实现
+
---
+++ main/JVM07HowReflect/Test.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Method;
+
+/**
+ * Created by Yangmingkai on 2021/4/27.
+ */
+public class Test {
+    public static void target(int i) {
+        new Exception("#" + i).printStackTrace();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Class klass = Class.forName("Test");
+        Method method = klass.getMethod("target", int.class);
+        method.invoke(null, 0);
+        for (int i = 0; i < 20; i++) {
+            method.invoke(null, i);
+        }
+    }
+}
